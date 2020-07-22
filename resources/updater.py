0cad4c2cb41e87367706c17b4315ad2f08d73d7d
TyReesh Boedhram <33663233+tboedhram@users.noreply.github.com>
2020-07-22 08:36:38 -0500

    Files successfully replaced with new versions.

--- resources/updater.py
+++ resources/updater.py
@@ -12,21 +12,36 @@ def clean_up():
     shutil.rmtree('new_version')
 
 
+def move_files(root_directory, directory):
+    files_in_directory = os.listdir(os.path.join(root_directory, directory))
+    for file in files_in_directory:
+        source = os.path.join(root_directory, directory, file)
+        destination = os.path.join(directory, file)
+        if os.path.isdir(source):
+            move_files(root_directory, os.path.join(directory, file))
+        else:
+            os.replace(source, destination)
+
+
 def update():
     with open('new_version.zip', 'wb') as new_version:
         request = requests.get('https://github.com/tboedhram/file-sync/zipball/master')
         new_version.write(request.content)
         new_version.close()
     with ZipFile('new_version.zip', 'r') as new_version_zip:
+        folder = new_version_zip.namelist()[0][:-1]
+        directory = os.path.join('new_version', folder)
         new_version_zip.extractall('new_version')
         new_version_zip.close()
-    # clean_up()
+    move_files(directory, '')
+    clean_up()
 
 
 def check_for_updates():
     with open('version.info', 'r') as version_info:
         current_version = version_info.read()
-        request = requests.get('https://raw.github.com/tboedhram/file-sync/v4.0-auto-updater/version.info')
-        github_version = request.text
-        if current_version != github_version:
-            updater_gui.gui(current_version, github_version)
+        version_info.close()
+    request = requests.get('https://raw.github.com/tboedhram/file-sync/v4.0-auto-updater/version.info')
+    github_version = request.text
+    if current_version != github_version:
+        updater_gui.gui(current_version, github_version)
